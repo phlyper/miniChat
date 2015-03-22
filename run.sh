291847ae1d03a594e2016bed270039368083a3db
phlyper <phlyper@live.fr>
2015-03-22 01:05:38 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
-gcc server_c.c -o server -lpthread
+# script compile and run the application
 
+# compile server code
+gcc server_c.c -o server -lpthread
+# compile client code
 javac -Xlint Client.java
+
+# run server
+gnome-terminal -e ./server
+# run client
+gnome-terminal -e java Client
 
